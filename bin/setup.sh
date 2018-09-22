8d1298fcffe04bc1e6f247a5685a2a680073128d
Adam Kerr <ajrkerr@gmail.com>
2018-09-22 13:32:35 -0400

    Install bundler

--- bin/setup.sh
+++ bin/setup.sh
@@ -3,6 +3,9 @@
 echo "Setting up database.yml"
 cp config/database.yml{.sample,}
 
+echo "Installing bundler"
+gem install bundler --no-ri --no-rdoc || panic "Error installing bundler"
+
 echo "Setting up Rails and Postgres"
 pushd api
 bundle install || panic "Error installing rails dependencies"
@@ -11,6 +14,9 @@ echo "Setting up database..."
 rake db:create db:schema:load 2>&1 > /dev/null || panic "Error running database related rake tasks, you should have a postgresql."
 
 
+echo ""
+echo ""
+echo ""
 echo "**********"
 echo "Setup finished successfully"
 echo "**********"
