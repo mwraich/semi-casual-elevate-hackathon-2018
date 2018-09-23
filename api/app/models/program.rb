941945d38645ed03e680e306bdaa81195bc1fdb0
Adam Kerr <ajrkerr@gmail.com>
2018-09-22 22:29:12 -0400

    Reorder the program lines

--- api/app/models/program.rb
+++ api/app/models/program.rb
@@ -1,11 +1,10 @@
 class Program < ApplicationRecord
-
-  acts_as_taggable
-
   include PgSearch
   pg_search_scope :search,
     against: [:title, :description],
     using: {
       tsearch: { dictionary: "english" }
     }
+
+  acts_as_taggable
 end
