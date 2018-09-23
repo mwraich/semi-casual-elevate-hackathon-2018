cfd3b915ec186d79559bb3bc66a2722165d7717e
Adam Kerr <ajrkerr@gmail.com>
2018-09-22 21:24:15 -0400

    Finish implementing searching

--- api/app/controllers/events_controller.rb
+++ api/app/controllers/events_controller.rb
@@ -1,5 +1,15 @@
 class EventsController < ApplicationController
   def index
-    render json: Program.all
+    render json: find_programs
+  end
+
+  private
+
+  def find_programs
+    if params[:q]
+      Program.search(params[:q])
+    else
+      Program.all
+    end
   end
 end
--- api/app/models/program.rb
+++ api/app/models/program.rb
@@ -1,4 +1,8 @@
 class Program < ApplicationRecord
   include PgSearch
-  multisearchable :against => [:title, :description]
+  pg_search_scope :search,
+    against: [:title, :description],
+    using: {
+      tsearch: { dictionary: "english" }
+    }
 end
