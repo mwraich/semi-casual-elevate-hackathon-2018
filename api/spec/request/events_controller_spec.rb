6377a1ada66a7c3ccffa2d47514e367af18727de
Adam Kerr <ajrkerr@gmail.com>
2018-09-22 21:24:15 -0400

    Finish implementing searching

--- api/spec/request/events_controller_spec.rb
+++ api/spec/request/events_controller_spec.rb
@@ -1,12 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe EventsController, type: :request do
-  it 'renders programs' do
+  it 'returns all events' do
     Program.create!(title: 'some event')
 
     get '/events'
 
     expect(records.map(&:title)).to include('some event')
+  end
+
+  context 'searching' do
+    it 'filters events by title' do
+      Program.create!(title: 'find me')
+      Program.create!(title: 'something else')
+
+      get '/events?q=find+me'
+
+      expect(records.map(&:title)).to include('find me')
+      expect(records.map(&:title)).to_not include('something else')
+    end
+
+    it 'filters events by description' do
+      Program.create!(description: 'find me')
+      Program.create!(description: 'something else')
+
+      get '/events?q=find+me'
+
+      expect(records.map(&:description)).to include('find me')
+      expect(records.map(&:description)).to_not include('something else')
+    end
   end
 
   private
